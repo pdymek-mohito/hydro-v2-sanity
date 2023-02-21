97cc4a34164b3e567e8433f11e62385b4c0ae542
Paweł Dymek <p.dymek@mohi.to>
2023-02-21 12:02:37 +0100

    Add Sanity types

--- app/lib/type.ts
+++ app/lib/type.ts
@@ -1,4 +1,5 @@
 import type {Storefront as HydrogenStorefront} from '@shopify/hydrogen';
+import type {Image} from '@sanity/types';
 import type {
   CountryCode,
   CurrencyCode,
@@ -28,3 +29,61 @@ export enum CartAction {
   UPDATE_BUYER_IDENTITY = 'UPDATE_BUYER_IDENTITY',
 }
 export type CartActions = keyof typeof CartAction;
+
+
+export type SanityHeroHome = {
+  content?: SanityImageWithProductHotspots | SanityProductWithVariant;
+  link?: SanityLink;
+  title?: string;
+};
+
+export type SanityImageWithProductHotspots = {
+  _key?: string;
+  _type: 'imageWithProductHotspots';
+  image: SanityAssetImage;
+  productHotspots: SanityProductHotspot[];
+};
+
+export type SanityProductWithVariant = {
+  _id: string;
+  _key?: string;
+  _type: 'productWithVariant';
+  available: boolean;
+  gid: string;
+  slug?: string;
+  variantGid: string;
+};
+
+export type SanityLink = SanityLinkExternal | SanityLinkInternal;
+
+export interface SanityAssetImage extends Image {
+  _type: 'image';
+  altText?: string;
+  blurDataURL: string;
+  height: number;
+  url: string;
+  width: number;
+}
+
+export type SanityLinkExternal = {
+  _key: string;
+  _type: 'linkExternal';
+  newWindow?: boolean;
+  url: string;
+  title: string;
+};
+
+export type SanityLinkInternal = {
+  _key: string;
+  _type: 'linkInternal';
+  documentType: string;
+  slug?: string;
+  title: string;
+};
+
+export type SanityProductHotspot = {
+  _key?: string;
+  product: SanityProductWithVariant;
+  x: number;
+  y: number;
+};
